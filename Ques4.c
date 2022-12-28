734eeef7cc85dc2a147bb172b5d13aa154674925
Divya Singh <divyasingh.300246@gmail.com>
2022-12-28 22:08:18 +0530

    updated

--- Ques4.c
+++ Ques4.c
@@ -4,9 +4,14 @@ double f(double x)
 {
     return 3*x-cos(x)-1;
 }
-double f1(double x)
+double df(double x)
 {
     return 3+sin(x);
+}
+
+double next(double x)
+{
+    return x-(f(x)/df(x));
 }
 double turncate(double x)
 {
@@ -14,34 +19,49 @@ double turncate(double x)
 			double z= (double)y/1000;
 			return z;
 }
-int isEqual(double arr[3])
+int isEqual(double v1,double v2,double v3)
 { 
-    if(turncate(arr[0])==turncate(arr[1])&&turncate(arr[1])==turncate(arr[2])&&turncate(arr[0])==turncate(arr[2])){return 1;}
+    if(turncate(v1)==turncate(v2)&&turncate(v2)==turncate(v3)&&turncate(v3)==turncate(v1)){return 1;}
     else
     {return 0;}
 }
 int main()
 {
-    double temp =0;
+    double x;
+    printf("\n enter the value of x0 :: ");
+    scanf("%lf",&x);
+    double v1=x;
+    double v2=next(v1);
+    double v3;
+    int itr =1;
     while(1)
     {
-        double arr[3];
-        printf("Enter value of x0 :\n");
-        scanf("%lf",&arr[1]);
-        int itr=1;
-        while(!isEqual(arr))
-        {
-          //  printf("f%lf=%lf \t f1 %lf=%lf \t %lf=%lf %lf\n",arr[0],f(arr[0]),arr[0],f1(arr[0]),arr[1],f(arr[1]),arr[2]);
-            arr[2]=arr[1]-(f(arr[1])/f1(arr[1]));
-                 arr[0]=arr[1];
-                arr[1]=arr[2];
-           
-            printf("\nITERATION NUMBER :%d",itr);
-             printf("\n%lf\n%lf\n%lf",arr[0],arr[1],arr[2]);
-             itr++;    
-        }
-       
-        printf("\nNumber of iterations:%d\nResult=%lf",itr,turncate(arr[0]));
-        break;
+       v3=next(v2);
+        printf("\n%lf\n%lf\n%lf",turncate(v1),turncate(v2),turncate(v3));
+        itr++;
         
+        if(isEqual(v1,v2,v3))  
+        {
+            break;
+        } 
+      
+      v1=next(v3);
+      itr++;
+        if(isEqual(v1,v2,v3))  
+        {
+             printf("\nNumber of iterations:%d\nResult=%lf",itr,turncate(v1));
+            break;
+        } 
+
+       v2=next(v1);
+       itr++;
+         if(isEqual(v1,v2,v3))  
+        {
+             printf("\nNumber of iterations:%d\nResult=%lf",itr,turncate(v2));
+            break;
+        } 
+    
+             
     }
+    printf("\nNumber of iterations:%d\nResult=%lf",itr,turncate(v3));
+}
